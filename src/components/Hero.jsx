fffdd16a4527f015a695d6387fc5dc05e000e7bf
Nota0515 <businessbro78@gmail.com>
2024-10-12 23:18:12 +0530

    added heroimage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ import { curve, heroBackground, robot } from "../assets";
 const Hero = () => {
   return (
     <Section
-    className='pt-[12rem] -mt-[5.25]'
+    className='pt-[12rem] -mt-[5.25rem]'
     crosses
     crossesOffset="lg:translate-y-[5.25rem]"
     customPaddings
@@ -28,6 +28,31 @@ const Hero = () => {
           <Button href="#pricing" white>
             Try for Free
           </Button>
+        </div>
+        <div className='relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24'>
+          <div className='relative z-1 p-0.5 rounded-2xl bg-conic-gradient'>
+            <div className='relative bg-n-8 rounded-[1rem]'>
+              <div className='h-[1.5rem] bg-n-10 rounded-t-[0.9rem]'/>
+               <div className='aspect-[33/40] rounded-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]'>
+                  <img 
+                    src={robot}
+                    className='w-full scale-[1.7] -translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]'
+                    width={1024}
+                    height={490}
+                    alt='heroImage'
+                  />
+               </div>
+            </div>
+          </div>
+          <div className='absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%] ' >
+            <img 
+              className='w-full'
+              src={heroBackground}
+              width={1440}
+              height={1800}
+              alt='herobg'
+            />
+          </div>
         </div>
       </div>
     </Section>
